37b05a09191d85f6fee24d8cd97b6dc1a4bfa942
Burhan Karataş <burhankaratas771@gmail.com>
2023-11-01 14:10:33 +0300

    Ana dizinin oluşturulması

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request,g
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators, SubmitField
+from passlib.hash import sha256_crypt 
+from functools import wraps
+import hashlib
+
+
+app = Flask(__name__)
+app.secret_key= "isdfgrhsri"
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
